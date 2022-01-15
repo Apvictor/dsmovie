47bcddff580d4610c375f3eb2752ef00446025bd
Armando Víctor Pereira <armandinho14.ap@gmail.com>
2022-01-15 11:23:42 -0300

    Pagination

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -25,15 +25,19 @@ function Listing() {
         axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}`)
             .then(response => {
                 const data = response.data as MoviePage
-                setPage(data);                
+                setPage(data);
             });
     }, [pageNumber]);
+
+    const handlePageChange = (newPageNumber: number) => {
+        setPageNumber(newPageNumber);
+    }
 
 
 
     return (
         <>
-            <Pagination />
+            <Pagination page={page} onChange={handlePageChange} />
 
             <div className="container">
                 <div className="row">
